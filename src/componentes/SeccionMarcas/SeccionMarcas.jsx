8ff53655567b1685514c56dba727e7aaa1ede622
hernanc77 <hernancarballo@hotmail.com>
2025-04-22 20:24:15 -0300

    version_3

--- src/componentes/SeccionMarcas/SeccionMarcas.jsx
+++ src/componentes/SeccionMarcas/SeccionMarcas.jsx
@@ -1,4 +1,8 @@
 import "./SeccionMarcas.css";
+import deye from "/src/assets/Logos/deye.png";
+import growatt from "/src/assets/Logos/growatt.png";
+import ezviz from "/src/assets/Logos/ezviz.png";
+import amarisolar from "/src/assets/Logos/amerisolar_logo.png";
 import { useNavigate } from 'react-router-dom';
 
 const SeccionMarcas = () => {
