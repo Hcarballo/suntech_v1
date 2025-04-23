3731c9d91556fbc00c3da3f2c053e280d8c5410a
hernanc77 <hernancarballo@hotmail.com>
2025-04-22 23:26:24 -0300

    version_4

--- src/componentes/SeccionOne/SeccionOne.jsx
+++ src/componentes/SeccionOne/SeccionOne.jsx
@@ -1,18 +1,8 @@
 import "./SeccionOne.css"
-import logo from "/src/assets/Logos/logo_SunTech.png";
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect } from 'react'
+
 
 const SeccionOne = () => {
-    const frase = "<<Soluciones solares, futuro sostenible>>"
-    useEffect(() => {
-        AOS.init({
-          duration: 1000,
-          once: true
-        });
-      }, []);
-    return (
+      return (
         <div className="seccionone" id="inicio">
            
         </div>
