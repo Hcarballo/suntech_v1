365cc172ede2ef247fc40d3775ee6ac74829a7df
hernanc77 <hernancarballo@hotmail.com>
2025-04-26 18:46:55 -0300

    version_28

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from "react";
 import "./NavBar.css";
-import logo from "../../assets/Logos/logo_SunTech_2.png";
+import logo from "../../assets/Logos/logo_suntech_2.png";
 import CardWidget from "../CardWidget/CardWidget";
 
 
