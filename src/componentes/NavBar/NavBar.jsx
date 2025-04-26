11aae35eea6d8027084b777ed8f0fc4f0325f0ec
hernanc77 <hernancarballo@hotmail.com>
2025-04-26 19:42:39 -0300

    version_33

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,10 +1,8 @@
 
 import { useState } from "react";
 import "./NavBar.css";
-//import logo from "../../assets/Logos/logo_suntech_2.png";
+import logo from "../../assets/Logos/logo_suntech_2.png";
 import CardWidget from "../CardWidget/CardWidget";
-
-const logo = "/assets/Logos/logo_suntech_2.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
