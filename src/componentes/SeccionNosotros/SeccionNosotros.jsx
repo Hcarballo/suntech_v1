3d0ce02a4ac2b1e6821202e51d86b1a67f2f1e57
hernanc77 <hernancarballo@hotmail.com>
2025-06-12 20:49:35 -0300

    version_9.4

--- src/componentes/SeccionNosotros/SeccionNosotros.jsx
+++ src/componentes/SeccionNosotros/SeccionNosotros.jsx
@@ -2,8 +2,8 @@ import "./SeccionNosotros.css"
 import iso_9001 from "../../assets/Logos/iso_9001.png";
 import iso_50001 from "../../assets/Logos/iso_50001.png";
 import iso_14001 from "../../assets/Logos/iso_14001.png";
-import Socio_1 from "../../assets/Nosotros/Jonatan.png"
-import Socio_2 from "../../assets/Nosotros/Hernan.png"
+//import Socio_1 from "../../assets/Nosotros/Jonatan.png"
+//import Socio_2 from "../../assets/Nosotros/Hernan.png"
 
 const SeccionNosotros = () => {
   return (
@@ -12,36 +12,9 @@ const SeccionNosotros = () => {
         <h2>Nuestro Staff</h2>
       </div>
       <div className="contenedor-flex">
-        {/* Columna izquierda: cards + texto */}
         <div className="columna-izquierda">
-          <div className="tarjetas">
-            <div className="seccionnosotros-card">
-              <div className="seccionnosotros-card-img-container">
-                <img
-                  src={Socio_1}
-                  alt="Foto del empleado"
-                  className="seccionnosotros-card-img"
-                />
-              </div>
-              <div className="seccionnosotros-card-body">
-                <h3 className="seccionnosotros-card-title">Ing. Jonatan Moran</h3>
-                <h5 className="seccionnosotros-card-description">Socio</h5>
-              </div>
-            </div>
-            <div className="seccionnosotros-card">
-              <div className="seccionnosotros-card-img-container">
-                <img
-                  src={Socio_2}
-                  alt="Foto del empleado"
-                  className="seccionnosotros-card-img"
-                />
-              </div>
-              <div className="seccionnosotros-card-body">
-                <h3 className="seccionnosotros-card-title">Ing. Hernán Carballo</h3>
-                <h5 className="seccionnosotros-card-description">Socio</h5>
-              </div>
-            </div>
-          </div>
+         
+          
           <div className="texto">
             <p>
               Diseñamos proyectos personalizados que se adapten perfectamente a tus necesidades. Desde el diseño inicial hasta la puesta en marcha, garantizamos un servicio integral que optimiza la eficiencia energética de tu hogar o empresa. Con un fuerte enfoque en la innovación y calidad, transformamos la energía solar en una opción accesible, rentable y beneficiosa para todos.
